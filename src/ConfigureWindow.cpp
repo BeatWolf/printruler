39a7289d66bb9ea2b53e81484f8d5a1d22ff73c6
asraniel@fryx.ch <asraniel@fryx.ch@759b87b5-ed4f-0410-b808-51908e403db0>
2008-12-24 12:24:00 +0000

    -minor fix

--- src/ConfigureWindow.cpp
+++ src/ConfigureWindow.cpp
@@ -3,7 +3,9 @@
 ConfigureWindow::ConfigureWindow()
 {
 	conf = Config::instance();
-	
+
+	setWindowTitle("Configure");
+
 	QLabel *label = new QLabel("Check directory");
 	checkFolder = new QLineEdit();
 	checkFolder->setText(*conf->getWatchFolder());
@@ -15,23 +17,23 @@ ConfigureWindow::ConfigureWindow()
 	layout->addWidget(label,0,0);
 	layout->addWidget(checkFolder,0,1);
 	layout->addWidget(selectFolder,0,2);
-	
+
 	QLabel *labelShow = new QLabel("Start in systray only");
 	showOnOpen = new QCheckBox();
 	showOnOpen->setChecked(!conf->getShowOnOpen());
 
 	layout->addWidget(showOnOpen,1,1);
 	layout->addWidget(labelShow,1,0);
-	
+
 	QPushButton *save = new QPushButton("Save");
 	connect(save, SIGNAL(clicked()), this, SLOT(save()));
 	QPushButton *cancel = new QPushButton("Cancel");
 	connect(cancel, SIGNAL(clicked()), this, SLOT(cancel()));
 	layout->addWidget(save,2,1);
-	layout->addWidget(cancel,2,2);	
-		
+	layout->addWidget(cancel,2,2);
+
 	setLayout(layout);
-	resize(400,100);	
+	resize(400,100);
 }
 
 ConfigureWindow::~ConfigureWindow()
@@ -41,12 +43,12 @@ ConfigureWindow::~ConfigureWindow()
 
 void ConfigureWindow::save(){
 	QDir dir(checkFolder->text());
-	
+
 	if(dir.exists()){
 		QString temp = checkFolder->text();
 		conf->setWatchFolder(temp);
 	}
-	
+
 	conf->setShowOnOpen(!showOnOpen->isChecked());
 	conf->saveConfigFile();
 	accept();
@@ -63,9 +65,9 @@ void ConfigureWindow::changeWatchFolder(){
             "/home",
             QFileDialog::ShowDirsOnly
             | QFileDialog::DontResolveSymlinks);
-	
+
 	QDir dir(fileName);
-		
+
 	if(dir.exists()){
 		checkFolder->setText(fileName);
 	}
